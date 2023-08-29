df5218d1ae5c64f6895d60ba349d8bc70c552d8c
Carlos Valverde <carlos.valverde@tusmedia.com>
2023-08-29 12:45:49 +0200

    [src] left menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import { BoxShadow } from "./pages/boxShadow";
 import { BorderRadiusPage } from "./pages/borderRadiusPage";
+import { BoxShadowPage } from './pages/boxShadowPage';
 
 function App() {
   return (
     <HashRouter future={{ v7_startTransition: true }}>
        <Routes>
         <Route path="/" element={<BorderRadiusPage/>}/> 
-        <Route path="/box-shadow/" element={<BoxShadow/>}/> 
+        <Route path="/box-shadow/" element={<BoxShadowPage/>}/> 
         <Route path="/border-radius/" element={<BorderRadiusPage/>}/> 
-        <Route path="/css-generator/" element={<BoxShadow/>}/>
-        <Route path="/css-generator/box-shadow" element={<BoxShadow/>}/>
+        <Route path="/css-generator/" element={<BoxShadowPage/>}/>
+        <Route path="/css-generator/box-shadow" element={<BoxShadowPage/>}/>
         <Route path="/css-generator/border-radius" element={<BorderRadiusPage/>}/>
       </Routes>
     </HashRouter>
